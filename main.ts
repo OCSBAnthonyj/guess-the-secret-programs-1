c3b5a6e86a8fec8dd1cadeca16d8ac9edff55ca2
OCSBAnthonyj <113910453+OCSBAnthonyj@users.noreply.github.com>
2022-10-04 09:15:10 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,91 @@
+input.onButtonPressed(Button.A, function () {
+    num += 1
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(num)
+    for (let index = 0; index < 3; index++) {
+        music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    num += -1
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("Goodbye")
+    control.reset()
+})
+let num = 0
+num = randint(-20, 20)
+for (let index = 0; index < 3; index++) {
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+}
